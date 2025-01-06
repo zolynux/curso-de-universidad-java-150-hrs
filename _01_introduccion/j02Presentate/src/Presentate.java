618b6f56295ba4a0ef9bb2b110f30074a18834a7
zolynux <152818486+zolynux@users.noreply.github.com>
2025-01-06 16:32:55 -0500

    Presentalo con Java

---
+++ _01_introduccion/j02Presentate/src/Presentate.java
@@ -0,0 +1,7 @@
+public class Presentate {
+    void main() {
+        System.out.println("Nombre: Juan");
+        System.out.println("Edad: 24 años");
+        System.out.println("Pais: Peru");
+    }
+}
